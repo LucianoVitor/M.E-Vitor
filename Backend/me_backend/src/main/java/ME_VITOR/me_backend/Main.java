ac8320d096e0007cf8be0209859701c9807aa807
oDippDev <carlosmanoel.dev@gmail.com>
2025-03-12 08:05:11 -0300

    Adição de Spring e organização para criação de entidade DB

---
+++ Backend/me_backend/src/main/java/ME_VITOR/me_backend/Main.java
@@ -0,0 +1,16 @@
+package ME_VITOR.me_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Main {
+
+	public static void main(String[] args) {
+
+
+		SpringApplication.run(Main.class, args);
+	}
+
+}
