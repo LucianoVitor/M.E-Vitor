59e16bf48898a42207d8e9e57ae37e09841feae3
oDippDev <carlosmanoel.dev@gmail.com>
2025-03-13 10:28:10 -0300

    refatoração de código e aprimoramento da validação

--- Frontend/JAVASCRIPT/events/passwordValidator.js
+++ Frontend/JAVASCRIPT/events/passwordValidator.js
@@ -2,17 +2,21 @@ const password = document.getElementById('password1');
 const confirmpassword = document.getElementById('password2');
 const submitbtn = document.getElementById('submitbutton');
 submitbtn.disabled=true
+const msg =document.getElementById('msgsenha')
+msg.style.display='none'
 
 function verificarSenha() {
   if (password.value === confirmpassword.value && password.value!=="" && confirmpassword.value!=="") {
     
     submitbtn.disabled = false;
     confirmpassword.style.borderColor=''
-
+    msg.style.display='none'
+        
   } else {
     
     submitbtn.disabled = true; 
     confirmpassword.style.borderColor='red'
+    msg.style.display='inline'
   
   }
 }
