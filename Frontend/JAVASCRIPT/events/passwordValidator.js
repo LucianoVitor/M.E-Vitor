50a57494f01fea4ecafc24a446d36a696697deb2
oDippDev <carlosmanoel.dev@gmail.com>
2025-03-12 22:55:05 -0300

    feat:Aprimoramento do sistema de validação de senha localmente

--- Frontend/JAVASCRIPT/events/passwordValidator.js
+++ Frontend/JAVASCRIPT/events/passwordValidator.js
@@ -1,18 +1,20 @@
 const password = document.getElementById('password1');
 const confirmpassword = document.getElementById('password2');
 const submitbtn = document.getElementById('submitbutton');
+submitbtn.disabled=true
 
-
-function verificarSenhas() {
-  if (password.value === confirmpassword.value) {
+function verificarSenha() {
+  if (password.value === confirmpassword.value && password.value!=="" && confirmpassword.value!=="") {
     
     submitbtn.disabled = false;
+    confirmpassword.style.borderColor=''
 
   } else {
     
     submitbtn.disabled = true; 
+    confirmpassword.style.borderColor='red'
   
   }
 }
-password.addEventListener('input', verificarSenhas);
-confirmpassword.addEventListener('input', verificarSenhas);
+password.addEventListener('input', verificarSenha);
+confirmpassword.addEventListener('input', verificarSenha);
