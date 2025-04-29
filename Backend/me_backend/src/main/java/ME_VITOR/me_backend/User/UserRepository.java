99fd85c0597547be9705383ee268ccc301321aff
oDippDev <carlosmanoel.dev@gmail.com>
2025-04-28 21:22:16 -0300

    chore: remove import não utilizado

--- Backend/me_backend/src/main/java/ME_VITOR/me_backend/User/UserRepository.java
+++ Backend/me_backend/src/main/java/ME_VITOR/me_backend/User/UserRepository.java
@@ -1,7 +1,6 @@
 package ME_VITOR.me_backend.User;
 
 import java.util.Optional;
-import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
